bfe7e83ebf3def170ff958994fb0ce8a37ecafbd
leonleo997 <yesid.lopez@correo.icesi.edu.co>
2018-04-16 23:45:26 -0500

    filter

--- src/app/components/busqueda/busqueda.component.ts
+++ src/app/components/busqueda/busqueda.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hotel } from '../../model/hotel';
+import { HotelService } from '../../services/hotel.service';
+import {Observable} from 'rxjs/Observable';
+import { equal } from 'assert';
+import { Subscribe } from '@firebase/util';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,10 +11,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./busqueda.component.css']
 })
 export class BusquedaComponent implements OnInit {
+hoteles: Hotel[];
+busqueda: Hotel[];
+nombre: string;
 
-  constructor() { }
+
+constructor(public hotelService: HotelService) {
+  this.busqueda = [{
+    id: '',
+    nombre: '',
+    costoHabitacion: 0,
+    latitud: 0,
+    longitud: 0
+  }];
+  this.hoteles = [{
+    id: '',
+    nombre: '',
+    costoHabitacion: 0,
+    latitud: 0,
+    longitud: 0
+  }];
+  this.nombre = '';
+
+}
 
   ngOnInit() {
+    this.hotelService.getHotels().subscribe(valores => this.hoteles = valores);
+    this.hotelService.getHotels().subscribe(valores => this.busqueda = valores);
+    this.nombre = '';
+  }
+
+  buscar() {
+    console.log('inicia buscar');
+    if ( this.nombre === '' ) {
+      this.nombre = ' ';
+    }
+    this.busqueda = this.busqueda.filter(hotel => hotel.nombre === this.nombre);
+    if ( (this.busqueda.length) === 0 ) {
+      this.busqueda = this.hoteles;
+    }
+    const m: number = this.busqueda.length;
   }
 
 }
--- src/app/components/nuevo-hotel/nuevo-hotel.component.ts
+++ src/app/components/nuevo-hotel/nuevo-hotel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hotel } from '../../model/hotel';
+import { HotelService } from '../../services/hotel.service';
 
 @Component({
   selector: 'app-nuevo-hotel',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo-hotel.component.css']
 })
 export class NuevoHotelComponent implements OnInit {
+  hotels: Hotel[];
+  hotel: Hotel;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(public hotelService: HotelService) {
+    this.hotel = {
+      id : '',
+      nombre: '',
+      costoHabitacion: 0,
+      latitud: 0,
+      longitud: 0
+    };
+    this.hotelService.getHotels().subscribe(hotels => {
+      this.hotels = hotels;
+    });
   }
 
+  ngOnInit() {
+    this.hotelService.getHotels().subscribe(hotels => {
+      this.hotels = hotels;
+    });
+  }
+
+  addHotel() {
+    this.hotelService.addHotel(this.hotel);
+  }
 }
